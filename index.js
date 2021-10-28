2987f5663ab0f2856a521dfba59b4b0cb4af52ee
brodie-m <brodiedmcguire@gmail.com>
2021-10-28 10:32:11 +0100

    added winner logic

--- index.js
+++ index.js
@@ -135,6 +135,10 @@ io.on("connection", async (socket) => {
     await User.updateOne({_id: payload.userId}, {$inc : {"stats.numGames": 1}})
     
 })
+  socket.on('add-winner', async (payload) => {
+    console.log('adding winner', payload.username)
+    await User.updateOne({_id: payload.userId}, {$inc: {"stats.numWins": 1}})
+  })
   socket.on("disconnect", async () => {
     const foundRooms = await Room.find({ participants: `${socket.id}` });
     foundRooms.forEach((room) => {
