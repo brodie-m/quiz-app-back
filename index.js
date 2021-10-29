a43bf1085768bd542f68d2014435bdf74a85d200
brodie-m <brodiedmcguire@gmail.com>
2021-10-29 09:54:04 +0100

    changing socket location

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Game = require("./models/Game");
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://inquizzy.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: [],
     credentials: true,
